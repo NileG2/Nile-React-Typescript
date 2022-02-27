8a5a7195c02e5168b940b9d827e7a5e1a5da68dd
Jayesh Singh <jayeshsingh001@gmail.com>
2022-02-27 13:43:14 +0530

    global scss added-app.scss

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
-import './App.scss';
+import "./App.scss";
 
 function App() {
   return (
     <div className="App">
-      <h1>Amazon Clone</h1>
+      <div className="bigHeader">hello</div>
+      <div className="header">hello</div>
+      <div className="subHeader">hello</div>
+      <div className="border"></div>
+      <input type="text" className="inputField bold" />
+      <button className="btn btnOrange">whatsup</button>
+      <div className="colorBox" style={{ background: "red" }}></div>
     </div>
   );
 }
