e31fd643717f95b6cc1c6a57399dbedc89d32df6
Jayesh Singh <jayeshsingh001@gmail.com>
2022-02-27 13:49:03 +0530

    added centralize style

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,7 @@ import "./App.scss";
 function App() {
   return (
     <div className="App">
-      <div className="bigHeader">hello</div>
-      <div className="header">hello</div>
-      <div className="subHeader">hello</div>
-      <div className="border"></div>
-      <input type="text" className="inputField bold" />
-      <button className="btn btnOrange">whatsup</button>
-      <div className="colorBox" style={{ background: "red" }}></div>
+      <div className="bigHeader centerAlign">Welcome to Nile</div>
     </div>
   );
 }
