648a6ddc1e2027ef9f8d5ffc38bd6c465a314a8f
aditya-dawadikar <adityadawadikar2000@gmail.com>
2022-02-28 16:09:36 +0530

    added firebase image upload service and bootstrap cdn

--- src/screens/test/Test.tsx
+++ src/screens/test/Test.tsx
@@ -1,7 +1,32 @@
-import React from "react";
+import React,{useState,useEffect} from "react";
+
+import {uploadFiles,deleteFile} from '../../services/imageUpload/imageUpload'
 
 const Test = () => {
-  return <div>Test</div>;
+
+  const [files,setfiles] = useState<any>([])
+  const [filename,setfilename] = useState<string>("")
+
+  function handleFileUpload(e:any){
+    setfiles(e.target.files)
+  }
+
+  function handleFileDelete(){
+    // console.log(filename)
+    deleteFile(filename)
+  }
+
+  function handleChange(e:any){
+    setfilename(e.target.value)
+  }
+
+  return <div>
+    <input type="file" onChange={(e)=>{handleFileUpload(e)}} multiple/>
+    <button onClick={()=>{uploadFiles(files)}} >Upload</button>
+    <br/>
+    <input onChange={(e)=>{handleChange(e)}}/>
+    <button onClick={()=>{handleFileDelete()}}>Delete</button>
+  </div>;
 };
 
 export default Test;
