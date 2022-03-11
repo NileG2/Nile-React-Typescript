96406ad85c98dad7c7527063955c8784d116a543
Jatin Sadhwani <Jatinsadhwani.1234@gmail.com>
2022-03-11 10:36:23 +0530

    Add Inventory

--- src/components/cardContainer/CardContainerGrid.tsx
+++ src/components/cardContainer/CardContainerGrid.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import ProductCardVertical from "../cards/productCards/ProductCardVertical";
+import ProductCardVerticalInventory from "../cards/productCards/ProductCardVerticalInventory";
 import "./CardContainerGrid.scss";
 
 const CardContainerGrid = (props: any) => {
@@ -8,6 +9,49 @@ const CardContainerGrid = (props: any) => {
   const totalPages = Math.ceil(props.products.length / pageSize);
   if (!props.products) {
     return <></>;
+  }
+  if(props.type==="seller"){
+    
+    return (
+      <div className="row cardDiv">
+        <div className="products std-card">
+          {props.products
+            .slice(pageNum * pageSize, (pageNum + 1) * pageSize)
+            .map((product: any, index: number) => {
+              return (
+                <div className="col-2" key={index}>
+                  <ProductCardVerticalInventory key={index} product={product} />
+                </div>
+              );
+            })}
+        </div>
+        <div className="paginationBar">
+          <button
+            className={`editPageNum std-btn ${
+              pageNum === 0 ? "btnGray" : "std-btnOrange"
+            }`}
+            disabled={pageNum === 0}
+            onClick={() => setPageNum(pageNum - 1)}
+          >
+            {"<"}
+          </button>
+          <span>
+            Page {pageNum + 1} of {totalPages}
+          </span>
+          <button
+            className={`editPageNum std-btn ${
+              pageNum === totalPages - 1 ? "btnGray" : "std-btnOrange"
+            }`}
+            disabled={pageNum === totalPages - 1}
+            onClick={() => setPageNum(pageNum + 1)}
+          >
+            {">"}
+          </button>
+        </div>
+      </div>
+    );
+
+
   }
   return (
     <div className="row cardDiv">
---
+++ src/components/cards/productCards/ProductCardVerticalInventory.tsx
@@ -0,0 +1,78 @@
+import React, { useState } from "react";
+import Rating from "@mui/material/Rating";
+import { useNavigate } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import axios from "axios";
+import { addItem } from "../../../redux/actions/Cart";
+import { toast } from "react-toastify";
+
+const ProductCardVerticalInventory = (props: any) => {
+  let auth = JSON.parse(sessionStorage.getItem("user") || "{}");
+  const navigate = useNavigate();
+
+
+ const handleToDelete=():void=>{
+    console.log("handle delete")
+ }
+
+  return (
+    <div className="Card">
+      <div className="std-card std-card-dimension std-no-shadow p-0 m-1">
+        <div className="d-flex justify-content-center">
+          <img
+            alt="productImg"
+            src={
+              props.product.images.length > 0
+                ? props.product.images[0]
+                : "https://picsum.photos/100"
+            }
+            width="100%"
+            height="100%"
+            style={{ cursor: "pointer" }}
+            onClick={() =>
+              navigate(
+                `/product/details?pid=${props.product.product_id}&category=${props.product.category}`
+              )
+            }
+          />
+        </div>
+        <div className="p-2">
+          <p
+            className="m-0 std-boldFont overflow-hidden"
+            style={{ cursor: "pointer" }}
+            onClick={() =>
+              navigate(`/product/details?pid=${props.product.product_id}`)
+            }
+          >
+            {props.product.name}
+          </p>
+          <p className="std-bold std-greenText m-0 std-desc">
+            {props.product.available_quantity > 0 ? (
+              "In Stock"
+            ) : (
+              <span className="std-redText">Out of Stock</span>
+            )}
+          </p>
+          <p className="std-fontSmall std-desc m-0">
+            Sold by: {props.product.brand || "Nile"}
+          </p>
+
+          {/* hardcoded rating */}
+          <Rating name="read-only" value={3.5} precision={0.5} readOnly />
+
+          <div className="row">
+            <p className="col-3 m-0 std-desc std-fontSmall">INR</p>
+            <p className="col-9 m-0 p-0 std-boldFont  std-font1">
+              {props.product.price}
+            </p>
+          </div>
+            <button className="std-btn std-btnOrange" onClick={() => { handleToDelete() }}>Delete</button>
+          
+
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ProductCardVerticalInventory;
