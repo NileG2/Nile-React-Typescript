50f324b33c0638844300b7dd0f672d4d62b32524
Jatin Sadhwani <Jatinsadhwani.1234@gmail.com>
2022-03-14 11:01:00 +0530

    Inventory issue solve

--- src/screens/sellerProfile/innerScreens/inventory/Inventory.tsx
+++ src/screens/sellerProfile/innerScreens/inventory/Inventory.tsx
@@ -6,31 +6,37 @@ import React, { useState, useEffect } from "react";
 import CardContainerGrid from "../../../../components/cardContainer/CardContainerGrid";
 import { useDispatch, useSelector } from "react-redux";
 import { getProductList } from "../../../../redux/actions/Product";
+import { CircleSpinner } from "react-spinners-kit";
 
 const Inventory = () => {
   const [addProductForm, setAddProductForm] = useState(true);
   const [Products,setProducts] = useState()
   const [MyProducts,setMyProducts] = useState<any>([])
+  const [loading,setLoading] = useState<any>(false)
   let auth = JSON.parse(sessionStorage.getItem("user") || "{}");
   let dispatch = useDispatch()
   let productList = useSelector((state:any)=>state.productDetail.productList)
 
   useEffect(() => {
-
+    setLoading(true)
     axios.post("http://localhost:9000/api/myproducts/", {
       userid: auth['userid']
     }).then(({ data }) => {
       setProducts(data.products)
-      // console.log(data.products)
       
       axios.post("http://localhost:9000/api/products/list", 
       data.products
     ).then(({ data }) => {
       setMyProducts(data.products)
-      console.log(data.products)
+      setLoading(false)
       dispatch(getProductList(data.products))
       
+    }).catch(err=>{
+      console.log(err)
     })
+    }).catch(err=>{
+      console.log(err)
+      setLoading(false)
     });
 
   }, []);
@@ -41,24 +47,17 @@ const Inventory = () => {
       <p className="std-font2">INVENTORY</p>
       <div className="std-section"></div>
       <div className="d-flex m-2">
-        {/* <div className="input-group">
-          <input
-            type="text"
-            className="std-inputField"
-            aria-label="Recipient's username"
-          />
-          <button className="std-btn std-btnOrange" type="button">
-            <FaSearch />
-          </button>
-        </div> */}
       </div>
       <br />
       {
+        loading === true ? <div className="loadingDiv">
+        <CircleSpinner size={30} color="#232f3e" />
+      </div> : <>{
         productList.length >0 ? <div><CardContainerGrid products = {productList} type={"seller"}/></div> : 
         <h2 className="std-subHeader">No items in inventory... Please add some products.</h2>
         
-      }
-      
+      }</>
+    }
     </div>
   );
 };
